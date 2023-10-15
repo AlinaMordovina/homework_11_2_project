f873353294deed5f62b0f2f4430bcc891f44668b
Alina Mordovina <alina182@yandex.ru>
2023-10-16 02:47:58 +0400

    Change str_func.py add function get_capitalize_word

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def get_upper_word(word):
     return word.upper()
+
+
+def get_capitalize_word(word):
+    """Функция, которая делает заглавными первые буквы каждого слова в строке."""
+    return word.capitalize()
